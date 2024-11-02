1ce5fcf8eb47a88f1747d03b49b98ee30067c2ec
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-11-02 16:37:49 +0100

    refactoring base of auth folder

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,4 +1,9 @@
+const authService = require('./auth.service');
 class AuthController {
+  #service;
+  constructor(){
+    this.#service = authService;
+  }
   async sendOTP(req, res, next) {
     try {
     } catch (error) {
